d7f2891078b1fef820bcb1f3b993005212f098d9
ibrahimhuseynov <tu7pdt8l8@code.edu.az>
2022-03-10 11:45:37 +0400

    aztu.edu.az

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main( string[] args)
+        {
+            Console.WriteLine(namesurname("azad a"));
+            Console.ReadLine();
+        }
+        static string namesurname( string name )
+        {
+            char one = ' ' ;
+            for (int i = 0; i <name.Length; i++)
+            {
+               if(name[i] == one)
+                {
+                  string newname = name-name[i];
+                    return newname;
+                }
+            }
+             
+        }
+    }
+}
